945fa641bb173717c5e9259996e2db3c5f4d1ac1
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-24 13:05:05 -0700

    task3

---
+++ 2-selection_sort.c
@@ -0,0 +1,66 @@
+#include "sort.h"
+
+/**
+ * min - returns the min of an array starting from
+ * a position and moving to the right
+ * @array: the array
+ * @size: array size
+ * @start_pos: search starting position
+ * Return: the index of the minimun value of the array
+ * from position start_pos onward
+*/
+size_t min(int *array, size_t size, size_t start_pos)
+{
+	size_t min;
+
+	size_t i = 0;
+	size_t current_min = start_pos;
+
+	for (i = start_pos; i < size; i++)
+	{
+		if (array[current_min] > array[i])
+		{
+			current_min = i;
+		}
+		;
+	}
+
+	return (current_min);
+}
+
+/**
+ * selection_sort - the sloppiest implementation of selection sort alg
+ * @array: array to e sorted
+ * @size: size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int tmp;
+	int array_i;
+	int array_min;
+	size_t minimum;
+	size_t min_idx;
+
+	for (i = 0; i < size; i++)
+	{
+
+
+		if (array[i] > array[min(array, size, i)])
+		{
+			minimum = array[min(array, size, i)];
+			min_idx = min(array, size, i);
+			array_i = array[i];
+			array_min = tmp;
+
+
+			tmp = array[i];
+
+			array[i] = minimum;
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+
+		;
+	}
+}
