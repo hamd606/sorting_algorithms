4f7e919d72c37de1efa2f80b7fd0cf5f583d0a33
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-24 13:25:51 -0700

    task3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,14 +5,12 @@
  * a position and moving to the right
  * @array: the array
  * @size: array size
- * @start_pos: search starting position
+ * @starting_pos: search starting position
  * Return: the index of the minimun value of the array
- * from position start_pos onward
+ * from position starting_pos onward
 */
 size_t min(int *array, size_t size, size_t start_pos)
 {
-	size_t min;
-
 	size_t i = 0;
 	size_t current_min = start_pos;
 
@@ -37,8 +35,6 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	int tmp;
-	int array_i;
-	int array_min;
 	size_t minimum;
 	size_t min_idx;
 
@@ -50,8 +46,7 @@ void selection_sort(int *array, size_t size)
 		{
 			minimum = array[min(array, size, i)];
 			min_idx = min(array, size, i);
-			array_i = array[i];
-			array_min = tmp;
+
 
 
 			tmp = array[i];
