8688b27bc4496d92fb3ca932a6697b53dd9078fe
CHIKHAOUI Mohamed <hamd000@live.fr>
2023-09-29 04:18:40 -0700

    adding_dummy_files_for_optional_tasks

---
+++ deck.h
@@ -0,0 +1,34 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void _swap_a_b(listint_t *upper, listint_t *lower, listint_t *a, listint_t *b);
+void adjust_list_head(listint_t **list);
+size_t min(int *array, size_t size, size_t start_pos);
+void selection_sort(int *array, size_t size);
+
+int lomuto_partition(int *array, int lower, int higher, size_t size);
+void quick_sort_recursion(int *array, int lower, int higher, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
